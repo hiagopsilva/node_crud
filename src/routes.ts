b9d7844171ec13c0ed6d45d122c862afd308d89f
Hiago Prates <hiagohps360@gmail.com>
2021-12-15 22:15:42 -0300

    feat: get video controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateCategoryController } from './controllers/CreateCategoryController
 import { CreateVideoController } from './controllers/CreateVideoController';
 import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { GetAllCategoriesController } from './controllers/GetAllCategoriesController';
+import { GetAllVideosController } from './controllers/GetAllVideoController';
 import { UpdateCategoryController } from './controllers/UpdateCategoryController';
 
 const routes = Router();
@@ -11,6 +12,8 @@ routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoriesController().handle);
 routes.delete('/categories/:id', new DeleteCategoryController().handle);
 routes.put('/categories/:id', new UpdateCategoryController().handle);
+
 routes.post('/videos', new CreateVideoController().handle);
+routes.get('/videos', new GetAllVideosController().handle);
 
 export default routes;
