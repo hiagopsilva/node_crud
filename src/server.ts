0ed84b79de705432037939664c567288ef4c7a23
Hiago Prates <hiagohps360@gmail.com>
2021-12-15 21:51:57 -0300

    feat: get all category

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
   console.log('🚀 Running...');
